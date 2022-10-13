989688bbeb7563291d824395d3771206de2abc38
KIT <KIT@DESKTOP-20POLUP>
2022-10-13 14:34:21 +0700

    Add project files.

---
+++ IPalgor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPalgor
+{
+    internal class Program
+    {
+        public static bool valid_ip(string ipString)
+        {
+            if (String.IsNullOrWhiteSpace(ipString))
+            {
+                return false;
+            }
+
+            string[] splitValues = ipString.Split('.');
+            if (splitValues.Length != 4)
+            {
+                return false;
+            }
+
+            byte tempForParsing;
+
+            return splitValues.All(r => byte.TryParse(r, out tempForParsing));
+        }
+        static void Main(string[] args)
+        {
+            bool result = valid_ip("192.0.2.146");
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
